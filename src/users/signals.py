2bb9506a7e0e4bf44429b18965e0050b03df2ca5
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-08-24 07:15:54 +0530

    auto store & create new user location

--- src/users/signals.py
+++ src/users/signals.py
@@ -2,10 +2,17 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import Profile
+from .models import Profile, Location
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(users=instance)
+
+@receiver(post_save, sender=Profile)
+def create_Profile_location(sender, instance, created, **kwargs):
+    if created:
+        Location.objects.create(profile=instance)
+        instance.location = profile_location
+        instance.save()
