ae049a7b326d21fd4d5b29ae70a2e283d8126e0f
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-08-27 16:51:45 +0530

    adding user urls and main urls

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path , include
-from main import urls as main_app_urls
+from django.urls import path, include
 from django.conf.urls.static import static
+
+from main import urls as main_app_urls
+from users import urls as users_app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(main_app_urls)) ## all urls include that present to main app
+    path('', include(main_app_urls)),
+    path('', include(users_app_urls)),
 ]
 
 if settings.DEBUG:
